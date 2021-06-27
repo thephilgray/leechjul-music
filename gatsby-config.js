d009d1a34b1502d2a660aa5e4e3f5c4048450c73
thephilgray <thephilgray@gmail.com>
2021-06-27 00:49:43 -0400

    fix with custom icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#0E5FCF`,
         theme_color: `##0E5FCF`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
