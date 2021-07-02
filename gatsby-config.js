d5fcedfaf9c08b3c00ae43a2063b5d148e4c093f
thephilgray <thephilgray@gmail.com>
2021-07-02 05:30:53 -0400

    fix bad hex color setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#0E5FCF`,
-        theme_color: `##0E5FCF`,
+        theme_color: `#0E5FCF`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
