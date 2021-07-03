43e03db91c2463b1992579fdec18287f6eba1286
thephilgray <thephilgray@gmail.com>
2021-07-03 10:32:56 -0400

    add youtube embed component

---
+++ src/components/youtubeEmbed.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// credit: https://kevinsimper.medium.com/full-width-youtube-embed-with-react-js-responsive-embed-509de7e7c3bf
+const YouTubeEmbed = ({ youtubeId, title }) => {
+  return (
+    <div
+      className="video"
+      style={{
+        position: "relative",
+        paddingBottom: "56.25%" /* 16:9 */,
+        paddingTop: 25,
+        height: 0
+      }}
+    >
+      <iframe
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%"
+        }}
+        title={title || youtubeId}
+        src={`https://www.youtube.com/embed/${youtubeId}`}
+        frameBorder="0"
+      />
+    </div>
+  );
+};
+export default YouTubeEmbed;
